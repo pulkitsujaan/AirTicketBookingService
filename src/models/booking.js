49d01d8e1cdba27b3d547b3786e7a885c4672c1b
pulkitsujaan <pulkitsujaan2708@gmail.com>
2026-01-05 18:52:16 +0530

    Added migration to add new columns to bookings table

--- src/models/booking.js
+++ src/models/booking.js
@@ -27,6 +27,16 @@ module.exports = (sequelize, DataTypes) => {
       allowNull:false,
       values:['InProcess', 'Booked', 'Cancelled'],
       defaultValue:'InProcess'
+    },
+    noOfSeates: {
+        type:DataTypes.INTEGER,
+        allowNull:false,
+        defaultValue:1
+    },
+    totalCost: {
+        type:DataTypes.INTEGER,
+        allowNull:false,
+        defaultValue:0
     }
   }, {
     sequelize,
